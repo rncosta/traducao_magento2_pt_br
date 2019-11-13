da2f0b1cd10ac7557b822145c6baa6fdc88971fb
Leonardo Marquine <leonardo@web2view.com.br>
2019-11-12 23:50:03 -0300

    Update packagist files

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
-    'rafaelcg_pt_br',
+    'lmarquine_pt_br',
     __DIR__
 );
