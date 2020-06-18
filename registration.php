107c7d9d04e7ca8255f3cc40e9df3349fc862b1a
rncosta <rudinei.costa@compasso.com.br>
2020-06-18 19:55:30 -0300

    ajustando configuração composer

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
-    'lmarquine_pt_br',
+    'rncosta_pt_br',
     __DIR__
 );
